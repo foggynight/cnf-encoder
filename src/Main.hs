67c4b826dda1b76a360672cdaf1e85f3e3782093
Robert Coffey <robertcoffey709@gmail.com>
2026-07-13 14:22:18 -0230

    update main: parse expression and print DIMACS CNF

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,22 @@
 module Main where
 
+import System.IO (hPutStrLn, stderr)
+
 import CNF
 import Expr
 import Parser
 
+main' :: Expr -> IO ()
+main' expr = do
+  hPutStrLn stderr $ "Expression: " ++ show expr
+  hPutStrLn stderr "DIMACS CNF:"
+  case exprToCnf expr of
+    Left err  -> hPutStrLn stderr err
+    Right cnf -> putStr $ dimacsCnf cnf
+
 main :: IO ()
 main = do
   input <- getContents
-  let tokens = lexStr input
-  print tokens
+  case parse input of
+    Left err   -> hPutStrLn stderr err
+    Right expr -> main' expr
